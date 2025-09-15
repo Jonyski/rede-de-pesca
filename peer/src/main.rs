6399a67fc0c7d26667af6b1f25b6cef7103ca994
Jonyski <geleiaeavela@gmail.com>
2025-09-14 23:52:35 -0300

    implements initial fishing system

--- peer/src/main.rs
+++ peer/src/main.rs
@@ -19,6 +19,8 @@
 use async_channel::{Receiver, Sender, unbounded};
 use async_dup::Arc;
 use clap::Parser;
+use owo_colors::Style;
+use rand::seq::IndexedRandom;
 use smol::{Async, Unblock, io, prelude::*};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::Mutex;
@@ -54,6 +56,148 @@ enum Event {
     Leave(SocketAddr),
     /// Um peer enviou uma mensagem
     Message(SocketAddr, String),
+    /// Usuário realizou a ação "pescar"
+    Pesca,
+}
+
+struct Fish {
+    abyssals: Vec<String>,
+    mythicals: Vec<String>,
+    legendaries: Vec<String>,
+    shiny: Vec<String>,
+    epics: Vec<String>,
+    rares: Vec<String>,
+    commons: Vec<String>,
+}
+
+impl Fish {
+    pub fn new() -> Self {
+        Fish {
+            // Peixes comuns: 50% de ocorrência
+            commons: vec![
+                String::from("sardinha"),
+                String::from("atum"),
+                String::from("dourada"),
+                String::from("cação"),
+                String::from("traíra"),
+                String::from("arenque"),
+                String::from("robalo"),
+                String::from("tambaqui"),
+                String::from("corvina"),
+                String::from("cavala"),
+                String::from("truta"),
+                String::from("pescada"),
+                String::from("pacu"),
+                String::from("lambari"),
+                String::from("merluza"),
+                String::from("anchova"),
+            ],
+            // Peixes raros: 25% de ocorrência
+            rares: vec![
+                String::from("pirarucu"),
+                String::from("tucunaré"),
+                String::from("salmão"),
+                String::from("bacalhau"),
+                String::from("pintado"),
+                String::from("betta"),
+                String::from("bagre"),
+                String::from("peixe-palhaço"),
+                String::from("garoupa"),
+                String::from("ouriço"),
+                String::from("peixe-cirurgião"),
+                String::from("peixe-borboleta"),
+                String::from("piranha"),
+            ],
+            // Peixes épicos: 15% de ocorrência
+            epics: vec![
+                String::from("robalo-gigante"),
+                String::from("peixe-pedra"),
+                String::from("carpa"),
+                String::from("poraquê"),
+                String::from("peixe-voador"),
+                String::from("baiacu"),
+                String::from("peixe-lua"),
+                String::from("água-viva"),
+            ],
+            // Peixes shiny: 6% de ocorrência
+            shiny: vec![
+                String::from("botia-beijadora"),
+                String::from("peixe-filhote-de-diabo"),
+                String::from("peixe-mão vermelho"),
+                String::from("peixe-anjo-rainha"),
+                String::from("peixe-lanterna"),
+                String::from("dragão-marinho-comum"),
+                String::from("peixe-leão"),
+                String::from("cavalo-marinho"),
+            ],
+            // Peixes míticos: 2.5% de ocorrência
+            legendaries: vec![
+                String::from("peixe-serra"),
+                String::from("marlin azul"),
+                String::from("peixe-espada"),
+                String::from("baleia-jubarte"),
+                String::from("enguia-pelicano"),
+                String::from("quimera"),
+                String::from("dragão-marinho-folhado"),
+            ],
+            // Peixes míticos: 1% de ocorrência
+            mythicals: vec![
+                String::from("peixe-napoleão"),
+                String::from("raia-jamanta"),
+                String::from("polvo-de-anéis-azuis"),
+                String::from("peixe-mandarim"),
+                String::from("peixe-morcego de lábios vermelhos"),
+            ],
+            // Peixes abissais: 0.5% de ocorrência
+            abyssals: vec![
+                String::from("peixe-bolha"),
+                String::from("peixe-pescador"),
+                String::from("peixe-olho-de-barril"),
+                String::from("lula-vampira-do-inferno"),
+                String::from("tubarão-duende"),
+                String::from("tubarão-elefante"),
+            ],
+        }
+    }
+}
+
+#[allow(unused_variables)]
+fn fish(fish_catalog: &Fish) -> String {
+    let fish = {
+        let mut rng = rand::rng();
+        let x = rand::random_range(1..=200);
+        match x {
+            1 => {
+                let fish = fish_catalog.abyssals.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<150, 0, 60>().bold().style(fish)
+            }
+            2..=3 => {
+                let fish = fish_catalog.mythicals.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<255, 130, 60>().bold().style(fish)
+            }
+            4..=8 => {
+                let fish = fish_catalog.legendaries.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<240, 200, 60>().bold().style(fish)
+            }
+            9..=20 => {
+                let fish = fish_catalog.shiny.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<255, 80, 135>().bold().style(fish)
+            }
+            21..=50 => {
+                let fish = fish_catalog.epics.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<160, 15, 230>().bold().style(fish)
+            }
+            61..=100 => {
+                let fish = fish_catalog.rares.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<80, 150, 255>().bold().style(fish)
+            }
+            _ => {
+                let fish = fish_catalog.commons.choose(&mut rng).unwrap();
+                Style::new().fg_rgb::<100, 255, 160>().bold().style(fish)
+            }
+        }
+    };
+    format!("Você pescou um(a) {}!\n", fish)
 }
 
 /// Lida com os Eventos emitidos, podendo fazer broadcast
@@ -61,6 +205,7 @@ async fn dispatch(
     streams: Arc<Mutex<Vec<Arc<Async<TcpStream>>>>>,
     receiver: Receiver<Event>,
     my_addr: SocketAddr,
+    fish_catalog: Fish,
 ) -> io::Result<()> {
     // Recebe eventos em loop
     while let Ok(event) = receiver.recv().await {
@@ -78,10 +223,14 @@ async fn dispatch(
                     (format!("* {} saiu\n", addr), Some(*addr))
                 }
                 Event::Message(addr, msg) => (format!("[{}] - {}\n", *addr, msg), Some(*addr)),
+                Event::Pesca => {
+                    let fishing_msg = fish(&fish_catalog);
+                    (fishing_msg, None)
+                }
             };
 
             // Criando uma lista apenas com os peers que devem receber a mensagem
-            let sender_addr = sender_addr_opt.unwrap();
+            let sender_addr = sender_addr_opt.unwrap_or(my_addr);
             let streams_to_write = streams_guard
                 .iter()
                 .filter(|s| {
@@ -99,18 +248,21 @@ async fn dispatch(
 
         // Se a mensagem for de outro peer, apenas exibimos ela
         // Mas, se a mensagem for nossa, fazemos um broadcast
-        if sender_addr != my_addr {
-            print!("{}", output);
-        } else if let Event::Message(_, msg) = &event {
-            // Adicionando um \n para o destinatário ler a mensagem como uma nova linha
-            let network_message = format!("{}\n", msg);
-            for mut stream in streams_to_write {
-                let message_to_send = network_message.clone();
-                smol::spawn(async move {
-                    // Enviando a mensagem bruta e como bytes
-                    stream.write_all(message_to_send.as_bytes()).await.ok();
-                })
-                .detach();
+        match &event {
+            Event::Message(_, msg) if sender_addr == my_addr => {
+                // Adicionando um \n para o destinatário ler a mensagem como uma nova linha
+                let network_message = format!("{}\n", msg);
+                for mut stream in streams_to_write {
+                    let message_to_send = network_message.clone();
+                    smol::spawn(async move {
+                        // Enviando a mensagem bruta e como bytes
+                        stream.write_all(message_to_send.as_bytes()).await.ok();
+                    })
+                    .detach();
+                }
+            }
+            _ => {
+                print!("{}", output);
             }
         }
     }
@@ -166,8 +318,10 @@ fn main() -> io::Result<()> {
         println!("Escutando no endereço {}", listener.get_ref().local_addr()?);
         println!("Bem vindo à Rede de Pesca!\nFique a vontade para pascar e conversar :)");
 
+        let fish_catalog = Fish::new();
+
         // Spawnando o dispatcher
-        smol::spawn(dispatch(streams.clone(), receiver, my_addr)).detach();
+        smol::spawn(dispatch(streams.clone(), receiver, my_addr, fish_catalog)).detach();
 
         // Spawnando uma thread que lê o stdin e envia as mensagens do usuário para o dispatcher
         let sender_stdin = sender.clone();
@@ -177,7 +331,11 @@ fn main() -> io::Result<()> {
             while let Some(Ok(line)) = lines.next().await {
                 // Enviando apenas mensagens não vazias
                 if !line.trim().is_empty() {
-                    sender_stdin.send(Event::Message(my_addr, line)).await.ok();
+                    if line == "$p" || line == "$pescar" {
+                        sender_stdin.send(Event::Pesca).await.ok();
+                    } else {
+                        sender_stdin.send(Event::Message(my_addr, line)).await.ok();
+                    }
                 }
             }
         })
